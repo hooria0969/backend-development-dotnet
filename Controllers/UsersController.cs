0092369724a2762dcb35ab4364dfceba8d823811
hooria0969 <hooriak466@gmail.com>
2025-07-30 17:10:41 +0500

    Initial commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,61 @@
+
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UserManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private static List<User> users = new();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetAllUsers()
+        {
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUserById(int id)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            if (user == null) return NotFound();
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public ActionResult<User> CreateUser([FromBody] User user)
+        {
+            user.Id = users.Count > 0 ? users.Max(u => u.Id) + 1 : 1;
+            users.Add(user);
+            return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, user);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(int id, [FromBody] User updatedUser)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            if (user == null) return NotFound();
+
+            user.Name = updatedUser.Name;
+            user.Email = updatedUser.Email;
+            user.Department = updatedUser.Department;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(int id)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            if (user == null) return NotFound();
+
+            users.Remove(user);
+            return NoContent();
+        }
+    }
+
+   
+}
---
+++ Models/Users.cs
@@ -0,0 +1,6 @@
+public class User {
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string Department { get; set; }
+}
